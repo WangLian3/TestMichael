21f5b30e8874d864fe43bc69552f45ea1a3fe6ef
WangLian3 <michael.wang@chinasystems.com>
2019-04-15 08:32:20 +0800

    commit

--- src/Test/ComparaTo.java
+++ src/Test/ComparaTo.java
@@ -7,15 +7,15 @@ public class ComparaTo {
 	public static void main(String[] args) {
 		Date d1 =new Date();
 		Date d2 = CalendarUtils.addSubMonth(d1, 1);
-//		System.out.println(d1);
-//		System.out.println(d2);
+		System.out.println(d1);
+		System.out.println(d2);
 		
 //		System.out.println(d1.compareTo(d1));
 		
 		String eventNo1 = "Michael.wang0031"; 
 		String eventNo2 = "Michael.wang002"; 
 		
-		System.out.println(eventNo1.compareTo(eventNo2));
+		System.out.println(d1.compareTo(d2));
 		
 		
 //		Iterator<Object> it = iterator();
---
+++ src/Test/Online.java
@@ -0,0 +1,37 @@
+package Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Online {
+	
+	public void test() {
+        Calendar c = Calendar.getInstance();
+
+        c.set(2016,5,4);
+        Date before =c.getTime();
+
+        c.set(2016,5,5);
+        Date now=c.getTime();
+
+        c.set(2016,5,6);
+        Date after=c.getTime();
+
+        //before早于now，返回负数，可用于判断活动开始时间是否到了
+        int compareToBefore=before.compareTo(now);
+        System.out.println("compareToBefore = "+compareToBefore);
+
+        int compareToIntNow=now.compareTo(now);
+        System.out.println("compareToIntNow = "+compareToIntNow);
+
+        //after晚于now，返回正数，可用于判断活动结束时间是否到了
+        int compareToIntAfter=after.compareTo(now);
+        System.out.println("compareToIntAfter = "+compareToIntAfter);
+    }
+
+	
+	public static void main(String[] args) {
+		Online o = new Online();
+		o.test();
+	}
+}
